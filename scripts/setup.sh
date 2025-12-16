32a3fdae9409dbe83f4bb8bc26515115709a8229
Rothimus <rothdearl@gmail.com>
2025-12-16 08:27:59 -0600

    Updating script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,4 @@
 
 # Install the required packages.
 pip3 install colorama --upgrade --user "$@"
-#pip3 install python-dateutil --upgrade --user "$@"
+pip3 install python-dateutil --upgrade --user "$@"
