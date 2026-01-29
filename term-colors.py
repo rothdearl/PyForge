6a7bc15cfc2eb44aa0c33bf0f45b9f06c490d651
Rothimus <rothdearl@gmail.com>
2026-01-29 11:39:24 -0600

    Updating demo sentence.

--- term-colors.py
+++ term-colors.py
@@ -17,7 +17,7 @@ def main() -> None:
     Run the program logic.
     """
     for index, (fg_color, bg_color) in enumerate(zip(ansi.COLORS_256, ansi.BG_COLORS_256)):
-        print(f"[{index:>3}]: {fg_color}FG Color{ansi.RESET}, {bg_color}BG Color{ansi.RESET}")
+        print(f"[{index:>3}]: {fg_color}The quick brown fox jumps{ansi.RESET} {bg_color}over the lazy dog{ansi.RESET}")
 
 
 if __name__ == "__main__":
