e6bf83e3a0b0a6fe847a4e55d8cd01c182d33fe5
Rothimus <rothdearl@gmail.com>
2026-01-29 08:52:21 -0600

    Updating constants.

--- cli/constants.py
+++ cli/constants.py
@@ -2,11 +2,19 @@
 Constants used throughout the command-line interface package.
 """
 
-import os
-from typing import Final
+import os as _os
+import sys as _sys
+from typing import Final as _Final
 
 # OS-related constants.
-OS_IS_POSIX: Final[bool] = os.name == "posix"
-OS_IS_WINDOWS: Final[bool] = os.name == "nt"
+OS_IS_LINUX: _Final[bool] = _sys.platform.startswith("linux")
+OS_IS_MAC: _Final[bool] = _sys.platform == "darwin"
+OS_IS_POSIX: _Final[bool] = _os.name == "posix"
+OS_IS_WINDOWS: _Final[bool] = _sys.platform == "win32"
 
-__all__ = ["OS_IS_POSIX", "OS_IS_WINDOWS"]
+__all__ = [
+    "OS_IS_LINUX",
+    "OS_IS_MAC",
+    "OS_IS_POSIX",
+    "OS_IS_WINDOWS"
+]
