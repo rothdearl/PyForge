b95aad5ad25a21071fc430bfdae515704ad1b52a
Rothimus <rothdearl@gmail.com>
2026-02-24 08:43:18 -0600

    Added author email to project file and build cleanup steps in bootstrap-user.sh.

--- scripts/bootstrap-user.sh
+++ scripts/bootstrap-user.sh
@@ -2,3 +2,7 @@
 
 # Install Pyr-CLI: use --break-system-packages on externally managed environments.
 python3 -m pip install ../ --user "$@"
+
+# Remove build files.
+rm -r ../build/
+rm -r ../pyr_cli.egg-info/
