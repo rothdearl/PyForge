b4c7bad3031a8f2dd04ef44e3e537199b5161f7a
Rothimus <rothdearl@gmail.com>
2026-03-08 10:37:29 -0500

    Updated how IS_WINDOWS is determined.

--- pyrcli/cli/os_info.py
+++ pyrcli/cli/os_info.py
@@ -7,7 +7,7 @@ from typing import Final
 IS_LINUX: Final[bool] = sys.platform.startswith("linux")
 IS_MACOS: Final[bool] = sys.platform == "darwin"
 IS_POSIX: Final[bool] = os.name == "posix"  # Covers BSD, Linux, and macOS.
-IS_WINDOWS: Final[bool] = sys.platform == "win32"
+IS_WINDOWS: Final[bool] = os.name == "nt"
 
 __all__: Final[tuple[str, ...]] = (
     "IS_LINUX",
